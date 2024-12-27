0c19e65efd5dfc3ff303c7e6453a6bbcdb4540df
yoon <kimyam1008@gmail.com>
2024-12-27 15:50:54 +0900

    전환 이벤트

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,30 @@
     <button @click="animateBlock">Animate</button>
   </div>
   <div class="container">
-    <transition name="para">
+    <transition 
+      name="para" 
+      @before-enter="beforeEnter" 
+      @enter="enter" 
+      @after-enter="afterEnter" 
+      @before-leave="beforeLeave"
+      @leave="leave"
+      @after-leave="afterLeave"
+    >
       <p v-if="paraIsVisible">This is only sometimes visible...</p>
     </transition>
     <button @click="toggleParagraph">Toggle Paragraph</button>
   </div>
-  <base-modal @close="hideDialog" v-if="dialogIsVisible">
+  <div class="container">
+    <transition name="fade-button" mode="out-in">
+      <button @click="showUsers" v-if="!usersAreVisible">Show Users</button>
+      <button @click="hideUsers" v-else>Hide Users</button>
+    </transition>
+  </div>
+  <base-modal @close="hideDialog" :open="dialogIsVisible">
     <p>This is a test dialog!</p>
     <button @click="hideDialog">Close it!</button>
   </base-modal>
+
   <div class="container">
     <button @click="showDialog">Show Dialog</button>
   </div>
@@ -25,9 +40,40 @@ export default {
       animatedBlock: false,
       dialogIsVisible: false,
       paraIsVisible: false,
+      usersAreVisible:false
     };
   },
   methods: {
+    beforeEnter(el){
+      console.log('beforeEnter');
+      console.log(el);
+    },
+    enter(el){
+      console.log('enter');
+      console.log(el);
+    },
+    afterEnter(el){
+      console.log('afterEnter');
+      console.log(el);
+    },
+    beforeLeave(el){
+      console.log('beforeLeave');
+      console.log(el);
+    },
+    leave(el){
+      console.log('leave');
+      console.log(el);
+    },
+    afterLeave(el){
+      console.log('afterLeave');
+      console.log(el);
+    },
+    showUsers(){
+      this.usersAreVisible = true;
+    },
+    hideUsers(){
+      this.usersAreVisible = false;
+    },
     animateBlock(){
       this.animatedBlock = true;
     },
@@ -97,7 +143,7 @@ button:active {
 }
 
 .para-enter-active {
-    animation: slide-scale 0.3s ease-out;
+    animation: slide-scale 2s ease-out;
   /* transition: all 0.3s ease-out; */
 }
 
@@ -118,6 +164,25 @@ button:active {
   /* opacity: 0;
   transform: translateY(30px); */
 }
+
+.fade-button-enter-from,
+.fade-button-leave-to {
+  opacity: 0;
+}
+
+.fade-button-enter-active {
+  transition: opacity 0.3s ease-out;
+}
+
+.fade-button-leave-active {
+  transition: opacity 0.3s ease-in;
+}
+
+.fade-button-enter-to,
+.fade-button-leave-from {
+  opacity: 1;
+}
+
 
 @keyframes slide-scale {
   0% {
