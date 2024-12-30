77d8ffa6e5da6f067f0d92521735899655d278fb
YOON <kimyam1008@gmail.com>
2024-12-30 11:07:00 +0900

    라우트 변경에 따른 애니메이션 설정

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,51 @@
 <template>
-  <div class="container">
-    <div class="block" :class="{animate: animatedBlock}"></div>
-    <button @click="animateBlock">Animate</button>
-  </div>
-  <div class="container">
-    <transition 
-      name="para" 
-      @before-enter="beforeEnter" 
-      @enter="enter" 
-      @after-enter="afterEnter" 
-      @before-leave="beforeLeave"
-      @leave="leave"
-      @after-leave="afterLeave"
-    >
-      <p v-if="paraIsVisible">This is only sometimes visible...</p>
-    </transition>
-    <button @click="toggleParagraph">Toggle Paragraph</button>
-  </div>
-  <div class="container">
-    <transition name="fade-button" mode="out-in">
-      <button @click="showUsers" v-if="!usersAreVisible">Show Users</button>
-      <button @click="hideUsers" v-else>Hide Users</button>
-    </transition>
-  </div>
-  <base-modal @close="hideDialog" :open="dialogIsVisible">
-    <p>This is a test dialog!</p>
-    <button @click="hideDialog">Close it!</button>
-  </base-modal>
-
-  <div class="container">
-    <button @click="showDialog">Show Dialog</button>
-  </div>
+    <router-view v-slot="slotProps">
+      <transition name="route" mode="out-in">
+        <component :is="slotProps.Component"></component>
+      </transition>
+    </router-view>
 </template>  
 
 <script>
+
 export default {
+
   data() {
     return { 
       animatedBlock: false,
       dialogIsVisible: false,
       paraIsVisible: false,
-      usersAreVisible:false
+      usersAreVisible:false,
+      enterInterval: null,
+      leaveInterval: null
     };
   },
   methods: {
+    enterCancelled(el){
+      console.log(el);
+      clearInterval(this.enterInterval);
+    },
+    leaveCancelled(el){
+      console.log(el);
+      clearInterval(this.leaveInterval);
+    },
     beforeEnter(el){
       console.log('beforeEnter');
       console.log(el);
+      el.style.opacity = 0;
     },
-    enter(el){
+    enter(el, done){
       console.log('enter');
       console.log(el);
+      let round = 1;
+      this.enterInterval = setInterval(() => {
+        el.style.opacity = round * 0.01;
+        round++;
+        if(round > 100){
+          clearInterval(this.enterInterval);
+          done();
+        }
+      }, 20);
     },
     afterEnter(el){
       console.log('afterEnter');
@@ -59,10 +54,21 @@ export default {
     beforeLeave(el){
       console.log('beforeLeave');
       console.log(el);
+      el.style.opacity = 1;
     },
-    leave(el){
+    leave(el, done){
       console.log('leave');
       console.log(el);
+      let round = 1;
+      this.leaveInterval = setInterval(() => {
+        el.style.opacity = 1 - round * 0.01;
+        round++;
+        if(round > 100){
+          clearInterval(this.leaveInterval);
+          done();
+        }
+      }, 20);
+
     },
     afterLeave(el){
       console.log('afterLeave');
@@ -137,33 +143,7 @@ button:active {
   animation: slide-fade 0.3s ease-out forwards;
 }
 
-.para-enter-from {
-  /* opacity: 0;
-  transform: translateY(-30px); */
-}
 
-.para-enter-active {
-    animation: slide-scale 2s ease-out;
-  /* transition: all 0.3s ease-out; */
-}
-
-.para-enter-to {
-  /* opacity: 1;
-  transform: translateY(0); */
-}
-
-.para-leave-from {
-  /* opacity: 1;
-  transform: translateY(0); */
-}
-.para-leave-active {
-  animation: slide-scale 0.3s ease-out;
-  /* transition: all 0.3s ease-in; */
-}
-.para-leave-to {
-  /* opacity: 0;
-  transform: translateY(30px); */
-}
 
 .fade-button-enter-from,
 .fade-button-leave-to {
@@ -183,6 +163,21 @@ button:active {
   opacity: 1;
 }
 
+.route-enter-from{}
+.route-enter-active{
+  animation: slide-scale 0.4s ease-out;
+}
+.route-enter-to{}
+
+.route-leave-from{
+
+}
+.route-leave-active{
+  animation: slide-scale 0.4s ease-in;
+}
+.route-leave-to{
+
+}
 
 @keyframes slide-scale {
   0% {
